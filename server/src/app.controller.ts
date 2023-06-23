af314f8958ba321f86e43028309a8057df29e5b1
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-23 15:56:14 +0900

    mongodb 연결하기 시도 중.

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
-import { UserService } from './app.service';
-import { CreateUserDto } from './app.service';
-import { User } from './app.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
@@ -10,21 +8,6 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-}
-
-@Controller('users')
-export class userController {
-  constructor(private readonly userService: UserService) {}
-
-  @Post()
-  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.userService.createUser(createUserDto);
-  }
-
-  @Get()
-  async findAllUsers(): Promise<User[]> {
-    return this.userService.findAllUsers();
   }
 }
 
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -8,17 +8,3 @@ export class AppService {
     return 'Hello World!';
   }
 }
-export class UserService {
-  constructor(@InjectModel('users') private userModel: Model<users>) {}
-
-  // 사용할 모델을 주입받는다. 'User'는 MongoDB의 컬렉션 이름이다.
-
-  async createUser(userDto: CreateUserDto): Promise<users> {
-    const createdUser = new this.userModel(userDto);
-    return createdUser.save();
-  }
-
-  async findAllUsers(): Promise<users[]> {
-    return this.userModel.find().exec();
-  }
-}
