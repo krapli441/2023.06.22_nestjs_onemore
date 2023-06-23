a3a191ee3d086b778910ab7415bbca8e0cedbf72
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-23 15:05:02 +0900

    mongodb에 데이터를 넣어보려고 시도 중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function App() {
   return (
     <>
-      <div>hello bitches</div>
+      <h1>gay</h1>
+      <form>
+        <input placeholder="성을 입력해주세요"></input>
+        <input placeholder="이름을 입력해주세요"></input>
+        <button type="submit" placeholder="전송"></button>
+      </form>
     </>
   );
 }
