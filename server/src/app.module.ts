9c7a8afac0de4a83f17902f2f05a45f9be424e6c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-24 09:05:41 +0900

    다시 시도해보기

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
-import { UserSchema } from './users/user.model'; // UserModel import
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, UserSchema], // UserModel을 providers 배열에 추가
+  controllers: [AppController],
+  providers: [AppService], // UserModel을 providers 배열에 추가
 })
 export class AppModule {}
